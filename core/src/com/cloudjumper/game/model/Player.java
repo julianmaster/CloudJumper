ae41ff4f4191bd76578ab813c566c1c20bda1015
Julien <julienm86230@hotmail.fr>
2018-02-26 00:38:10 +0100

    Start player input management

--- core/src/com/cloudjumper/game/model/Player.java
+++ core/src/com/cloudjumper/game/model/Player.java
@@ -1,5 +1,7 @@
 package com.cloudjumper.game.model;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.cloudjumper.game.view.Assets;
 
@@ -7,5 +9,22 @@ public class Player extends Entity {
 
 	public Player(Body body, Assets asset) {
 		super(body, asset);
+	}
+
+	public void inputUpdate(float delta) {
+		int horizontalForce = 0;
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			horizontalForce -= 1;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			horizontalForce += 1;
+		}
+
+		if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+			getBody().applyForceToCenter(0, 50, false);
+		}
+
+		getBody().setLinearVelocity(horizontalForce * 3, getBody().getLinearVelocity().y);
 	}
 }
--- core/src/com/cloudjumper/game/view/GameScreen.java
+++ core/src/com/cloudjumper/game/view/GameScreen.java
@@ -11,9 +11,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.cloudjumper.game.CloudJumper;
 import com.cloudjumper.game.Constants;
-import com.cloudjumper.game.model.Cloud;
-import com.cloudjumper.game.model.Level;
-import com.cloudjumper.game.model.LevelGenerator;
+import com.cloudjumper.game.model.*;
 import com.cloudjumper.game.utils.TextureManager;
 
 public class GameScreen extends ScreenAdapter {
@@ -22,11 +20,11 @@ public class GameScreen extends ScreenAdapter {
 	private Level level;
 	private World world;
 
-	private boolean showDebugPhysics = false;
+	private boolean showDebugPhysics = true;
+	private boolean showBackground = true;
 	private Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 
-	private Body player;
-	private Body platform;
+	private Player player;
 
 	public GameScreen(CloudJumper game) {
 		this.game = game;
@@ -35,8 +33,7 @@ public class GameScreen extends ScreenAdapter {
 	@Override
 	public void show() {
 		world = new World(new Vector2(0, -9.8f), false);
-//		player = EntityManager.createBox(64, 64, 5, 8, false, world);
-//		platform = EntityManager.createBox(64, 16, 16*6, 16, true, world);
+		player = new Player(EntityManager.createBox(64, 64, 5, 8, false, world), null);
 		level = LevelGenerator.generateLevel(world);
 	}
 
@@ -57,9 +54,16 @@ public class GameScreen extends ScreenAdapter {
 			}
 			level = LevelGenerator.generateLevel(world);
 		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
+			showBackground = !showBackground;
+		}
+
+		player.inputUpdate(delta);
 
 		batch.begin();
-		batch.draw(TextureManager.getTexture(Assets.BACKGROUND.ordinal()), 0, 0);
+		if(showBackground) {
+			batch.draw(TextureManager.getTexture(Assets.BACKGROUND.ordinal()), 0, 0);
+		}
 		for(Cloud[] lines : level.getClouds()) {
 			for(Cloud cloud : lines) {
 				if(cloud != null) {
